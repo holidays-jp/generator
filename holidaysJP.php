164ceacae74808c4fac1614d44dd054328e693b3
matsuoshi <matsuoshi@gmail.com>
2016-12-05 16:06:09 +0900

    date.json と datetime.json 形式に変更

--- holidaysJP.php
+++ holidaysJP.php
@@ -6,11 +6,20 @@ class holidaysJP
     protected $ical_url;
     const DIST = __DIR__ . '/json/';
 
+
+    /**
+     * holidaysJP constructor.
+     * @param $url
+     */
     public function __construct($url)
     {
         $this->ical_url = $url;
     }
 
+
+    /**
+     * generate
+     */
     public function generate()
     {
         $data = $this->get_ical();
@@ -18,6 +27,11 @@ class holidaysJP
         $this->output_json($main_data);
     }
 
+
+    /**
+     * @param $data
+     * @return array
+     */
     function generate_main_json($data)
     {
         $dates = [];
@@ -37,10 +51,7 @@ class holidaysJP
                 continue;
             }
 
-            $dates[$datetime] = [
-                'date' => date('Y-m-d', $datetime),
-                'title' => $summary[1],
-            ];
+            $dates[$datetime] = $summary[1];
         }
 
         // 日付順にソートして返却
@@ -48,22 +59,43 @@ class holidaysJP
         return $dates;
     }
 
+
+    /**
+     * @param $data
+     */
     function output_json($data)
     {
-        file_put_contents(self::DIST . 'index.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-
+        $data_date = [];
         $yearly = [];
+        $yearly_date = [];
 
-        foreach ($data as $key => $a) {
-            $y = date('Y', strtotime($a['date']));
-            $yearly[$y][$key] = $a;
+        foreach ($data as $key => $value) {
+            $y = date('Y', $key);
+            $ymd = date('Y-m-d', $key);
+
+            $data_date[$ymd] = $value;
+            $yearly[$y][$key] = $value;
+            $yearly_date[$y][$ymd] = $value;
         }
 
+        file_put_contents(self::DIST . 'datetime.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+        file_put_contents(self::DIST . 'date.json', json_encode($data_date, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+
         foreach ($yearly as $year => $data) {
-            file_put_contents(self::DIST . "year/{$year}.json", json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+            $dir = self::DIST . $year;
+            if (! is_dir($dir)) {
+                mkdir($dir);
+            }
+
+            file_put_contents(self::DIST . $year . '/datetime.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+            file_put_contents(self::DIST . $year . '/date.json', json_encode($yearly_date[$year], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
         }
     }
 
+
+    /**
+     * @return mixed
+     */
     function get_ical()
     {
         // iCal データの取得
--- tests/holidaysJPTest.php
+++ tests/holidaysJPTest.php
@@ -8,18 +8,9 @@ class holidaysJPTest extends PHPUnit_Framework_TestCase
         $main_data = $holidays->generate_main_json($data);
 
         $expected = [
-            1420038000 => [
-                'date' => '2015-01-01',
-                'title' => '元日',
-            ],
-            1458486000 => [
-                'date' => '2016-03-21',
-                'title' => '春分の日 振替休日',
-            ],
-            1513954800 => [
-                'date' => '2017-12-23',
-                'title' => '天皇誕生日',
-            ],
+            1420038000 => '元日',
+            1458486000 => '春分の日 振替休日',
+            1513954800 => '天皇誕生日',
         ];
         $this->assertEquals($expected, $main_data);
     }
@@ -32,14 +23,20 @@ class holidaysJPTest extends PHPUnit_Framework_TestCase
 
         $dist = dirname(__DIR__) . '/json';
 
-        // index.json
-        $file_name = "{$dist}/index.json";
+        // date.json
+        $file_name = "{$dist}/date.json";
         $this->assertFileExists($file_name);
         $data = json_decode(file_get_contents($file_name));
         $this->assertGreaterThanOrEqual(1, count($data));
 
-        // this year json
-        $file_name = "{$dist}/year/" . date('Y') . ".json";
+        // datetime.json
+        $file_name = "{$dist}/datetime.json";
+        $this->assertFileExists($file_name);
+        $data = json_decode(file_get_contents($file_name));
+        $this->assertGreaterThanOrEqual(1, count($data));
+
+        // this year date.json
+        $file_name = "{$dist}/" . date('Y') . "/date.json";
         $this->assertFileExists($file_name);
         $data = json_decode(file_get_contents($file_name));
         $this->assertGreaterThanOrEqual(1, count($data));
