6c8cb81edb92d3316bca7a140babcb514ced1491
Nao Kawano <kawano@tam-tam.co.jp>
2017-02-03 19:55:40 +0900

    テストコードでcsvファイルフォーマットもチェックする

--- tests/holidaysJPTest.php
+++ tests/holidaysJPTest.php
@@ -40,15 +40,21 @@ class holidaysJPTest extends PHPUnit_Framework_TestCase
         $year = Carbon::now()->year;
         $this->checkApiFile('date.json', $year);
         $this->checkApiFile('datetime.json', $year, true);
+        $this->checkApiFile('date.csv', $year);
+        $this->checkApiFile('datetime.csv', $year, true);
 
         // 年別データのチェック (今年)
         $this->checkApiFile("{$year}/date.json", $year);
         $this->checkApiFile("{$year}/datetime.json", $year, true);
+        $this->checkApiFile("{$year}/date.csv", $year);
+        $this->checkApiFile("{$year}/datetime.csv", $year, true);
 
         // 年別データのチェック (来年)
         $nextyear = $year + 1;
         $this->checkApiFile("{$nextyear}/date.json", $nextyear);
         $this->checkApiFile("{$nextyear}/datetime.json", $nextyear, true);
+        $this->checkApiFile("{$nextyear}/date.csv", $nextyear);
+        $this->checkApiFile("{$nextyear}/datetime.csv", $nextyear, true);
     }
 
     /**
@@ -61,9 +67,38 @@ class holidaysJPTest extends PHPUnit_Framework_TestCase
     {
         // ファイルの存在チェック
         $filename = dirname(__DIR__) . "/json/{$filename}";
+
         $this->assertFileExists($filename);
 
-        $data = json_decode(file_get_contents($filename), true);
+        $fileChkArr = explode(".", $filename);
+        $fileExtension = end($fileChkArr);
+        $allowExtensions = array('json', 'csv');
+
+        $this->assertContains($fileExtension, $allowExtensions);
+
+        if ($fileExtension == 'json') {
+            $data = json_decode(file_get_contents($filename), true);
+        } else {
+            // 行で分けたcsvの配列を作る
+            // [[日付のcsv文字列],[祝日名のcsv文字列]]
+            $csvArrByLine = str_getcsv(file_get_contents($filename), "\n");
+
+            // 行ごとのcsv文字列を配列にする
+           // [[0]=>[[0]=>'YYYYMMDD',[1]=>'YYYYMMDD'..],[1]=>[[0]=>'祝日名'..]]
+            foreach($csvArrByLine as $csvLine) {
+                $dateTextArr[] = str_getcsv($csvLine);
+            }
+
+            // $dateTextArr[0]の値 .. 日付一覧
+            // $dateTextArr[1]の値 .. 祝日名一覧
+            $cnt = count($dateTextArr[0]);
+            $this->assertCount($cnt, $dateTextArr[1]);
+
+            $dates = array_values($dateTextArr[0]);
+            foreach ($dates as $key => $date) {
+                $data[$date] = $dateTextArr[1][$key];
+            }
+        }
 
         // 元日のデータが入っているか
         $dt = Carbon::createFromDate($year)->startOfYear();
